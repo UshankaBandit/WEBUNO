bb111e79c38f98b3a8729df6f09d8b2e588db018
toften05 <jacob.toft2001@gmail.com>
2024-09-09 15:09:13 +0200

    test

---
+++ PlayingCard.ts
@@ -0,0 +1,15 @@
+
+
+export class PlayingCard {
+    
+    public Color  = ["Black" , "Green" , "Yellow" , "Red" , "Blue"]
+    public type = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0","+2","+4wild","wild", "skip","reverse"]
+
+    constructor(Color, type) {
+        Color = this.Color
+        type = this.type
+    }
+
+
+}
+
