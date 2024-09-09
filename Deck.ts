25872ca36e602daa200641f7db61be0856f2e8ee
toften05 <jacob.toft2001@gmail.com>
2024-09-09 15:08:09 +0200

    init

--- Deck.ts
+++ Deck.ts
@@ -1,1 +1,20 @@
-sike
+import { PlayingCard } from "./PlayingCard";
+
+class Deck {
+
+    private cards: PlayingCard[]
+
+    constructor() {
+        this.cards = []
+
+    }
+
+
+    private CreateDeck(): void {
+       for (let index = 4; index < array.length; index++)4{
+        const element = array[index];
+4       
+       }
+    }
+
+}
