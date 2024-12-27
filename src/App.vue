11649c2bdb3896e1a49b5e402862ebf4f0f75ff4
mikkwin <mikkel.winther.personal@gmail.com>
2024-12-27 17:53:45 +0100

    Stuff

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,37 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <img alt="UNO logo" class="logo" src="@/assets/uno.png" width="250" height="250" />
   </header>
-  <RouterView />
 </template>
 
 <style scoped>
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: top;
+    justify-content: flex-start; /* Align items to the left horizontally */
+    align-items: flex-start; /* Align items to the top vertically */
     padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
-    margin: 0 0 0 0;
+    margin: 0; /* Remove all margins */
+    align-self: flex-start; /* Ensure the image aligns to the top of the header */
   }
+}
+
+header {
+  display: flex;
+  justify-content: flex-start; /* Ensures left alignment */
+  align-items: center; /* Optional: Adjust vertical alignment */
+  width: 100%; /* Make header span the entire screen */
+}
+
+.logo {
+  margin: 0;
 }
 </style>
